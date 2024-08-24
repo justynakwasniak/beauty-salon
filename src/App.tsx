9072ccb4b5106e90451d938ca9b531273db432f5
Justyna Kwaśniak <jkwasniak193@gmail.com>
2024-08-24 11:38:57 +0200

    1ver of about me

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Services from "./components/Services";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
+import AboutUs from "./components/AboutUs";
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <AboutUs />
       <Services />
       <Team />
       <Contact />
---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import "../styles/AboutUs.css";
+
+import aboutus1 from "../assets/aboutus1.jpg";
+import aboutus2 from "../assets/aboutus2.jpg";
+
+const AboutUs: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
+  return (
+    <section className="about-us-container">
+      <div className="container">
+        <div className="row">
+          {/* Lewa strona ze zdjęciami */}
+          <div className="col-md-6">
+            <div className="about-us-images">
+              <img
+                src={aboutus1}
+                alt="Image 1"
+                className="img-fluid image-style"
+              />
+              <img
+                src={aboutus2}
+                alt="Image 2"
+                className="img-fluid image-style"
+              />
+            </div>
+          </div>
+
+          {/* Prawa strona z tekstem i przyciskiem */}
+          <div className="col-md-6">
+            <h2>O nas</h2>
+            <p>
+              Jesteśmy profesjonalnym salonem urody, który od lat dba o piękno
+              naszych klientów. Nasz zespół składa się z doświadczonych
+              specjalistów, którzy z pasją podchodzą do każdej metamorfozy.
+              Zarezerwuj wizytę już dziś i pozwól nam zadbać o Ciebie.
+            </p>
+            <Button variant="primary" onClick={handleShowModal}>
+              Rezerwacja Online
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Modal z formularzem */}
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Zostaw swoje dane</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formName">
+              <Form.Label>Imię</Form.Label>
+              <Form.Control type="text" placeholder="Wpisz swoje imię" />
+            </Form.Group>
+
+            <Form.Group controlId="formPhone">
+              <Form.Label>Telefon</Form.Label>
+              <Form.Control
+                type="tel"
+                placeholder="Wpisz swój numer telefonu"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formMessage">
+              <Form.Label>Wiadomość</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Wpisz swoją wiadomość"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Zamknij
+          </Button>
+          <Button variant="primary">Wyślij</Button>
+        </Modal.Footer>
+      </Modal>
+    </section>
+  );
+};
+
+export default AboutUs;
